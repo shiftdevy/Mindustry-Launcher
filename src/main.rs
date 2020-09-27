9a5dba77560c2614c8611d24fabfb9fabd5a2886
shift_develope <53366878+shiftdevy@users.noreply.github.com>
2020-09-27 16:49:22 +0300

    Dependencies + test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use] extern crate serde_derive;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let point = Point { x: 1, y: 2 };
+
+    let serialized = serde_json::to_string(&point).unwrap();
+
+    println!("serialized = {}", serialized);
+
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+
+    println!("deserialized = {:?}", deserialized);
 }
